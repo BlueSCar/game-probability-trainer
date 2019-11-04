d1e5a718a7f2d6ba942327ad03167ded416a20f3
BlueSCar <radjewwj@gmail.com>
2019-11-03 22:34:12 -0500

    trained up model

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-const amqplib = require('amqplib');
 const bluebird = require('bluebird');
 const dotenv = require('dotenv');
 const pgp = require('pg-promise');
 
 const dbConfig = require('./lib/database');
-const rabbitConfig = require('./lib/rabbit');
-const consumersConfig = require('./lib/consumers');
+const trainerConfig = require('./lib/trainer');
 
 (async() => {
     dotenv.config();
 
     const db = dbConfig(bluebird, pgp);
-    const rabbit = await rabbitConfig(amqplib);
+    const trainer = trainerConfig(db);
 
-    await consumersConfig(rabbit.channel);
+    // await trainer.generateTrainingData();
+
+    
+    const games = await db.any(`
+        SELECT g.id, gt.id AS game_team1_id, gt2.id AS game_team2_id
+        FROM game AS g
+            INNER JOIN game_team AS gt ON g.id = gt.game_id AND gt.home_away = 'home'
+            INNER JOIN game_team AS gt2 ON g.id = gt2.game_id AND gt.id <> gt2.id
+        WHERE g.season = 2015 AND gt.points IS NOT NULL AND gt2.points IS NOT NULL AND gt.win_prob IS NULL AND gt2.win_prob IS NULL
+    `);
+
+    for (let game of games) {
+        let result = await trainer.evaluateGame(game.id);
+        if (!result){
+            continue;
+        }
+
+        await db.tx(async t => {
+            await t.batch([
+                t.none('UPDATE game_team SET win_prob = $1 WHERE id = $2', [result, game.game_team1_id]),
+                t.none('UPDATE game_team SET win_prob = $1 WHERE id = $2', [1 - result, game.game_team2_id])
+            ]);
+        });
+    }
 })().catch(err => {
     console.error(err);
 });
